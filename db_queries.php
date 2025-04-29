07f9a702682071cd2cb7ae29dc90a3d581eb3970
angelic-coder19 <angelyuzya7@gmial.com>
2025-04-30 00:15:07 +0200

    Add File That Creates Connection to the database and Creates a Table

---
+++ db_queries.php
@@ -0,0 +1,41 @@
+<?php 
+    // Set up environment variables to hold database credentials
+    $server_name = "localhost";
+    $user_name = "root";
+    $password = "";
+    $db_name = "myDB"; 
+
+    // create connection 
+    $conn = mysqli_connect($server_name, $user_name, $password, $db_name);
+
+    // Check if connection is okay
+    if (!$conn)
+    {
+        echo "Connection failed: " . mysqli_connect_error($conn);
+    } else {
+        echo "Connection successful"; 
+    }
+
+    /*
+    // Create a table 
+    $sql = "CREATE TABLE Guestbook
+           (
+            id INT(6) AUTO_INCREMENT PRIMARY KEY,
+            full_name VARCHAR(100) NOT NULL,
+            email VARCHAR(50) NOT NULL, 
+            comment TEXT NOT NULL,
+            reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+           )";
+            
+    if (mysqli_query($conn, $sql))
+    {
+        echo "<br> Table created successfully!";
+    } else {
+        echo "<br> Error Creating table: " . mysqli_error($conn);
+    } */  
+    
+ 
+
+    // Close connection 
+    //mysqli_close($conn);
+?>
